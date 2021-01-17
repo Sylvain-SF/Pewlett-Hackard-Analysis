8b88b5ef595210ff45a35b3fb434ae1f1c178da4
Sylvain-SF <sylvain.dessagnes@gmail.com>
2021-01-17 11:55:30 -0800

    Add Folders

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,18 @@
+-- Retrive desired columns from Employees and Titles tables:
+
+SELECT
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no ;
+
+SELECT * FROM retirement_titles
+LIMIT 10 ;
